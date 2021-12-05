0386cc51852b21a3b7e80a270b09e48e34111f40
赵益军 <zhaoyijun@css.com.cn>
2021-12-05 13:05:49 +0800

    chore: init local env

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,13 @@ module.exports = {
   rules: {
     // 这里添加一行规则把这条规则隐藏
     'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'no-restricted-syntax': 0,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'no-sequences': 0,
+    'no-loop-func': 0,
+    'no-nested-ternary': 0,
   },
 };
